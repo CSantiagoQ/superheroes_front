22c9f001af178a74a97af507467f9e7942b0df8c
CSantiagoQ <182813678+CSantiagoQ@users.noreply.github.com>
2026-05-17 11:56:26 -0700

    feat: Initial Setup

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+﻿import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+    {
+        ignores: ['dist/**', '.angular/**', 'node_modules/**', 'coverage/**'],
+    },
+    {
+        files: ['**/*.ts'],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'app',
+                    style: 'camelCase',
+                },
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'app',
+                    style: 'kebab-case',
+                },
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.html'],
+        extends: [...angular.configs.templateRecommended],
+        rules: {},
+    },
+    eslintConfigPrettier,
+);
+
